21f5242a27ff1fccd178faedf583b5407ea805a8
shruti <shruti.mannikeri@gartner.com>
2023-04-17 15:09:18 +0530

    emptroved and added delete and

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 import express from "express"; // "type": "module"
 import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv"; // to import .env file
+const app = express();
+const PORT = 4000;
 dotenv.config();
 
 
-const app = express();
-const PORT = 4000;
+
 //mongo conection code
 
 const MONGO_URL = process.env.MONGO_URL;
@@ -69,6 +70,35 @@ app.get("/movies/:id", async function (request, response) {
     .db("Movieapp")
     .collection("movies")
     .findOne({ id: id });
+  //find will return perticular one
+
+  //if nmovie not found we have to set status code 404
+  mv
+    ? response.send(mv)
+    : response.status(404).send({ msg: "movie not found" });
+});
+
+//delet movie
+app.delete("/movies/:id", async function (request, response) {
+  const { id } = request.params;
+ 
+  const result = await client
+    .db("Movieapp")
+    .collection("movies")
+    .deleteOne({ id: id });
+    result.deletedCount > 0
+    ? response.send({msg: "Movie successfully deleteted"})
+    : response.status(404).send({ msg: "movie not found" });
+});
+
+//update movie
+app.put("/movies/:id", async function (request, response) {
+  const { id } = request.params;
+  const data = request.body;
+  const mv = await client
+    .db("Movieapp")
+    .collection("movies")
+    .updateOne({ id: id },{$set: data});
   //find will return perticular one
 
   //if nmovie not found we have to set status code 404
